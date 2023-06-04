c1043560bd175dd30088230851c83f4837dd0fe3
Isak Bamiani <90834593+Isak-Bamiani@users.noreply.github.com>
2023-06-04 14:43:13 +0200

    Add files via upload
    Initial spring boot backend application

---
+++ BE/src/main/java/com/example/skuld/SkuldApplication.java
@@ -0,0 +1,13 @@
+package com.example.skuld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkuldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkuldApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/com/example/skuld/SkuldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skuld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkuldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
